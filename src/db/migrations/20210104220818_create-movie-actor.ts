cd4c67a15fde746f5627073e381c497e47d5e8dd
Amado <amadob2b@gmail.com>
2021-01-04 23:26:05 +0000

    fix(migrations): typo

--- src/db/migrations/20210104220818_create-movie-actor.ts
+++ src/db/migrations/20210104220818_create-movie-actor.ts
@@ -3,20 +3,19 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.raw(`
-    CREATE TABLE MovieActor (
+    CREATE TABLE movie_actor (
       id    INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+      plays VARCHAR(50),
       movie    INT(10) UNSIGNED NOT NULL,
-      actor    INT(10) UNSIGNED NOT NULL
-      character  VARCHAR(50) NOT NULL,
-
-      CONSTRAINT PK_MovieActor__id PRIMARY KEY (id),
-      CONSTRAINT FK_movie FOREIGN KEY (id) REFERENCES Movie(id)
-      CONSTRAINT FK_actor FOREIGN KEY (id) REFERENCES Actor(id)
+      actor    INT(10) UNSIGNED NOT NULL,
+      CONSTRAINT PK_movie_actor__id PRIMARY KEY (id),
+      CONSTRAINT FK_movie FOREIGN KEY (id) REFERENCES movie(id),
+      CONSTRAINT FK_actor FOREIGN KEY (id) REFERENCES actor(id)
   );`)
 }
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.raw('DROP TABLE MovieActor;')
+    await knex.schema.raw('DROP TABLE movie_actor;')
 
 }
 
--- src/db/migrations/20210104221337_add-genre-to-movie.ts
+++ src/db/migrations/20210104221337_add-genre-to-movie.ts
@@ -3,19 +3,28 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.raw(`
-    ALTER TABLE Movie
-    ADD genre INT(10) UNSIGNED NOT NULL,
-    ADD CONSTRAINT FK_genre,
-FOREIGN KEY (genre) REFERENCES Genre(id);
-  );`)
+    ALTER TABLE movie
+  ADD
+    genre INT(10) UNSIGNED NOT NULL
+
+  ;`)
+
+  await knex.schema.raw(`
+  ALTER TABLE movie
+  ADD CONSTRAINT FK_genre
+  FOREIGN KEY (genre) REFERENCES genre(id)
+;`)
 }
 
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.raw(`
-    ALTER TABLE Movie
-     DROP CONSTRAINT FK_genre,
-     DROP COLUMN genre`)
+    ALTER TABLE movie
+     DROP CONSTRAINT FK_genre`)    
+     
+    await knex.schema.raw(`
+    ALTER TABLE movie
+    DROP COLUMN genre`)
 
 }
 
